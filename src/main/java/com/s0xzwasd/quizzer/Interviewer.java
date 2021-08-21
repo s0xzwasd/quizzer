803de3cfa8df4a94782a87155f0fd0ce7538325e
Daniil Maslov <i@dmaslov.dev>
2021-08-21 14:00:32 +0300

    Implement questions adding and listing

--- src/main/java/com/s0xzwasd/quizzer/Interviewer.java
+++ src/main/java/com/s0xzwasd/quizzer/Interviewer.java
@@ -1,5 +1,6 @@
 package com.s0xzwasd.quizzer;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Interviewer extends User {
@@ -7,7 +8,9 @@ public class Interviewer extends User {
         System.out.println("Please, wait a bit...");
 
         System.out.println("Successfully logged as " + user.getNameOfRole().toLowerCase() + "!");
-        System.out.println("Available commands: list, add, remove, exit");
+        System.out.println("Available commands: list, add, remove (TBD), exit");
+
+        ArrayList<Question> questions = new ArrayList<>();
 
         String command = "";
         Scanner scanner = new Scanner(System.in);
@@ -17,10 +20,51 @@ public class Interviewer extends User {
 
             switch (command) {
                 case "list":
-                    System.out.println("Questions and answers list");
+                    // TODO: Refactor and move it to a different method.
+                    if (!questions.isEmpty()) {
+                        System.out.println("Questions and answers list:");
+
+                        for (Question q : questions) {
+                            System.out.println(q);
+                        }
+                    } else {
+                        System.out.println("Currently the list of questions is empty.");
+                    }
+
                     break;
                 case "add":
-                    System.out.println("Add a new question");
+                    // TODO: Refactor add move it to a different method.
+                    System.out.println("Please, specify a question:");
+                    Scanner scannerQ = new Scanner(System.in);
+                    String scanQuestion = scannerQ.nextLine();
+
+                    System.out.println("Question is specified. Please, specify a correct answer:");
+                    // TODO: Investigate how to avoid Scanner spawning commands.
+                    Scanner scannerA = new Scanner(System.in);
+                    String correctAnswer = scannerA.nextLine();
+
+                    System.out.println("Correct answer is specified. Please, specify other answers (type end to " +
+                            "finish):");
+
+                    ArrayList<String> answers = new ArrayList<>();
+
+                    String answersCommand = "";
+
+                    do {
+                        Scanner scannerAC = new Scanner(System.in);
+                        answersCommand = scannerAC.nextLine();
+
+                        if (answersCommand.equals("end")) {
+                            System.out.println("Back to the main menu. Available commands: list, add, remove, exit");
+                        } else {
+                            answers.add(answersCommand);
+                            System.out.println("Answer is added successfully! Type end to close it.");
+                        }
+                    } while (!answersCommand.equals("end"));
+
+                    Question question = new Question(scanQuestion, correctAnswer, answers);
+                    questions.add(question);
+                    System.out.println("Question successfully added.");
                     break;
                 case "remove":
                     System.out.println("Remove a question");
---
+++ src/main/java/com/s0xzwasd/quizzer/Question.java
@@ -0,0 +1,23 @@
+package com.s0xzwasd.quizzer;
+
+import java.util.ArrayList;
+
+public class Question {
+    private String question;
+    private String correctAnswer;
+    private ArrayList<String> answerList = new ArrayList<>();
+
+    Question(String question, String correctAnswer, ArrayList<String> answers) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+
+        for (int i = 0; i < answers.size(); i++) {
+            answerList.add(answers.get(i));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Question: " + question + "\n Correct answer is " + correctAnswer + ".\n Other answers are: " + answerList.toString();
+    }
+}
